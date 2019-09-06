df29d06b5994fc0a5b2d0c5458f163250a27b968
Ozan Kasikci <kasikciozan@yandex.com>
2019-09-07 00:59:52 +0200

    parsetree: add parseNode

--- parser/parse_tree.go
+++ parser/parse_tree.go
@@ -2,6 +2,45 @@ package parser
 
 import "github.com/ozankasikci/ist/lexer"
 
+type ParseNode interface {
+	Where() lexer.Span
+}
+
 type ParseTree struct {
 	Source *lexer.SourceFile
+	Nodes []ParseNode
+}
+
+type baseNode struct {
+	where lexer.Span
+}
+
+func (v *baseNode) Where() lexer.Span                { return v.where }
+
+type LocatedString struct {
+	Where lexer.Span
+	Value string
+}
+
+func NewLocatedString(token *lexer.Token) LocatedString {
+	return LocatedString{ Where: token.Where, Value: token.Contents }
+}
+
+type DeclNode interface {
+	ParseNode
+}
+
+type baseDecl struct {
+	baseNode
+}
+
+type TypeDeclNode struct {
+	baseDecl
+	Name LocatedString
+	Type ParseNode
+}
+
+func (pt ParseTree) AddNode(node ParseNode)  {
+	pt.Nodes = append(pt.Nodes, node)
+
 }
