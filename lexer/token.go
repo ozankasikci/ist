b70cef8f69b0b620f7dcde72ebb6a1271e7b7211
Ozan Kasikci <kasikciozan@yandex.com>
2019-09-07 00:57:44 +0200

    lexer: token: add Span

--- lexer/token.go
+++ lexer/token.go
@@ -13,10 +13,28 @@ const (
 type Token struct {
 	Type     TokenType
 	Contents string
+	Where Span
 }
 
 type Position struct {
 	Filename string
 
 	Line, Char int
+}
+
+type Span struct {
+	Filename string
+
+	StartLine, StartChar int
+	EndLine, EndChar int
+}
+
+func NewSpan(start, end Position) Span {
+	return Span{
+		Filename: start.Filename,
+		StartLine: start.Line,
+		StartChar: start.Char,
+		EndLine: end.Line,
+		EndChar: end.Char,
+	}
 }
