831f35a0979a8753051e2753ca49d5db6f05a3f0
Ozan Kasikci <kasikciozan@yandex.com>
2019-09-08 15:33:47 +0200

    add parser_test

---
+++ parser/parser_test.go
@@ -0,0 +1,20 @@
+package parser
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	"github.com/ozankasikci/ist/lexer"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	sourceFile, err := lexer.NewSourceFile("../cmd/test.ist")
+	assert.NoError(t, err)
+
+	tokens := lexer.Lex(sourceFile)
+	sourceFile.Tokens = tokens
+	spew.Dump(tokens)
+
+	parseTree := Parse(sourceFile)
+	assert.NotNil(t, parseTree)
+}
